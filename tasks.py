9ded158198be31d9be80233d57b4319513b998a9
Weesoi <sokrime@outlook.com>
2024-10-30 13:35:47 +0200

    aaaaaaaaaaaaa

--- tasks.py
+++ tasks.py
@@ -1,6 +1,46 @@
-from robocorp.tasks import task
+from RPA.Browser.Selenium import Selenium
+from bs4 import BeautifulSoup
+import csv
+from RPA.Email.ImapSmtp import ImapSmtp
 
-@task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+# Vaihe 1: Selaimen avaus ja työpaikkasivulle siirtyminen
+browser = Selenium()
+browser.open_available_browser("https://jobly.fi")
+
+html_content = browser.get_page_source()
+soup = BeautifulSoup(html_content, "html.parser")
+job_listings = soup.find_all("div", class_="job-listing")
+
+jobs = []
+for job in job_listings:
+    title = job.find("h2").text
+    location = job.find("span", class_="location").text
+    if "työharjoittelu" in title.lower() and "Helsinki" in location:
+        jobs.append({
+            "title": title,
+            "location": location,
+            "link": job.find("a")["href"]
+        })
+
+with open("matching_jobs.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerow(["Title", "Location", "Link"])
+    for job in jobs:
+        writer.writerow([job["title"], job["location"], job["link"]])
+
+mail = ImapSmtp()
+mail.authorize("smtp.gmail.com", "email@example.com", "password")
+mail.send_message(
+    sender="email@example.com",
+    recipients=["student@example.com"],
+    subject="Sopivat työpaikkailmoitukset",
+    body="Liitteenä CSV sopivista työpaikoista.",
+    attachments=["matching_jobs.csv"]
+)
+
+browser.input_text("xpath://input[@name='first_name']", "Opiskelijan Nimi")
+browser.input_text("xpath://input[@name='last_name']", "Sukunimi")
+browser.upload_file("xpath://input[@type='file']", "CV.pdf")
+browser.click_button("xpath://button[@type='submit']")
+
+browser.close_browser()
