bffdd686cfc728dd92838b236851ad2d99543363
Weesoi <sokrime@outlook.com>
2024-11-01 15:27:37 +0200

    dodi

--- tasks.py
+++ tasks.py
@@ -1,25 +1,6 @@
-from RPA.Browser.Selenium import Selenium
-from bs4 import BeautifulSoup
-import csv
-from RPA.Email.ImapSmtp import ImapSmtp
 from robocorp.tasks import task
 
 @task
-def Jobly():
-    browser = Selenium()
-    browser.open_available_browser("https://jobly.fi")
-
-    # Yritetään hyväksyä evästeet CSS-valitsimella
-    try:
-        browser.wait_until_element_is_visible("css:button.message-component.message-button.no-children.focusable.alma-cmp-button.sp_choice_type_11", timeout=10)
-        browser.click_button("css:button.message-component.message-button.no-children.focusable.alma-cmp-button.sp_choice_type_11")
-    except:
-        print("Evästepopupia ei löytynyt tai sitä ei tarvinnut hyväksyä.")
-
-    # Jatka robotin toimintaa tästä eteenpäin...
-    # browser.input_text("xpath://input[@name='first_name']", "Opiskelijan Nimi")
-    # browser.input_text("xpath://input[@name='last_name']", "Sukunimi")
-    # browser.upload_file("xpath://input[@type='file']", "CV.pdf")
-    # browser.click_button("xpath://button[@type='submit']")
-
-    # browser.close_browser()
+def minimal_task():
+    message = "Hello"
+    message = message + " World!"
